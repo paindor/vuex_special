e90b015377c55fc081d2093287b94acf14c0ac46
최광진 <53929412+paindor@users.noreply.github.com>
2020-01-22 18:36:33 +0900

    first commit

---
+++ src/components/Todo.vue
@@ -0,0 +1,41 @@
+<template>
+<div>
+    <div>counter</div>
+   {{ getCount}} <br />
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+</div>
+</template>
+
+<script>
+    import { mapGetters } from 'vuex'
+export default {
+  name: 'counter',
+/*
+  data(){
+      return{
+          counter: getCount()
+
+
+      }
+  },*/
+    methods:{
+      increment(){
+          this.$store.dispatch('increment')
+
+
+      },
+        decrement(){
+            this.$store.dispatch('decrement')
+
+            alert('-')
+        }
+    },
+    computed: {
+        ...mapGetters([
+            'getCount'
+        ]),
+    }
+
+};
+</script>
---
+++ src/store/counter.js
@@ -0,0 +1,37 @@
+const state ={
+    count: 0
+
+}
+const mutations ={
+    increment(state){
+        state.count++
+    },
+    decrement(state){
+        state.count--
+    }
+
+}
+
+const actions={
+
+
+    increment:({commit}) => commit('increment'),
+    decrement:({commit})=> commit('decrement')
+
+
+}
+const getters={
+    getCount: function (state) {
+        return state.count;
+    }
+
+}
+export default {
+    name: 'counter',
+    namespace: true,
+    state,
+    getters,
+    actions,
+    mutations
+
+}
